c345ed84d791cd9f71009c6bd426afc7791309df
Petr <je09@yandex.ru>
2026-06-05 01:06:18 +0500

    feat: lock app orientation to portrait

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react'
 import { useSpotifyAuth } from './features/auth/useSpotifyAuth'
 import { AppProviders } from './app/AppProviders'
 import { AppShell } from './app/AppShell'
 
 export function App() {
   const { token, logout } = useSpotifyAuth()
+
+  useEffect(() => {
+    const lockPortrait = () => {
+      const orientation = screen.orientation as ScreenOrientation & { lock?: (orientation: 'portrait') => Promise<void> }
+      void orientation.lock?.('portrait').catch(() => {})
+    }
+
+    lockPortrait()
+    document.addEventListener('visibilitychange', lockPortrait)
+    return () => document.removeEventListener('visibilitychange', lockPortrait)
+  }, [])
 
   return (
     <AppProviders token={token}>
